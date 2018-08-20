595b6c051cfea47a0b077392cfb5d76fb342b874
Maxim Lopatnikov <maxim.alp@gmail.com>
2018-08-20 13:59:25 -0400

    ignore updates

--- router/keys.js
+++
@@ -1,9 +0,0 @@
-
-spotifyConfig = {
-  clientId: '0772407bf77c48499a6561083a06ab9c',
-  clientSecret: 'b76a41f8fbcc47c3852cc53916110880',
-  playlistIdIndie: '2HpEuQZMms7dfQ3NjG2V4u',
-  playlistIdPop: '7p4l5NxeOyY0pwhcgvnVvN'
-};
-
-module.exports = spotifyConfig;
